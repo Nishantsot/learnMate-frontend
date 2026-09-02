2e2579acae72f9e50e8560584a0aeef2b23823ce
Nishant Kumar <nishantkumar8357@gmail.com>
2026-09-02 09:38:29 +0530

    Fix Vercel SPA refresh routes

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,20 +1,27 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log("API URL:", API_URL);
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
